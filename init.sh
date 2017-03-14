7a05f7a948ab713637a4ce83069ed4d4ab4dca35
shuizhongyueming <shuizhongyueming@gmail.com>
2017-03-14 23:03:26 +0800

    add init script for UltiSnips

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+# add config
 ln .gitconfig ~/.gitconfig
 ln .vimrc ~/.vimrc
 ln .zshrc ~/.zshrc
+# add snpis for vim
+mkdir -p ~/.vim/UltiSnips/
+ln UltiSnips/html.snippets ~/.vim/UltiSnips/html.snippets
+ln UltiSnips/javascript.snippets ~/.vim/UltiSnips/javascript.snippets
+ln UltiSnips/less.snippets ~/.vim/UltiSnips/less.snippets
